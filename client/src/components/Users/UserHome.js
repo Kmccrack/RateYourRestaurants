9c707e6411bec2967ad50b86fd58863f33dbd0b1
Kat <m_kaitavia@yahoo.com>
2019-01-29 22:53:17 -0500

    more css

--- client/src/components/Users/UserHome.js
+++ client/src/components/Users/UserHome.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 
 const GeneralStyles = styled.div`
     text-align: center;
+    font-family: 'Playfair Display SC', serif;
 `
 
 class UserHome extends Component {
@@ -29,19 +30,44 @@ class UserHome extends Component {
 
     render() {
         return (
-            <GeneralStyles>
+            <GeneralStyles >
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <Link to={`/users`} style={{color: 'red'}} activeStyle={{color: 'white'}}><h1>RateYourRaunts</h1></Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
                 <h1>Create Your User & Get Started</h1>
-                <button onClick={this.toggleNewUserForm}>Create new user</button>
+                   <div className='userHome'>
+                {this.state.users.map((user, i) => (
+                    <div className="container">
+                    <div className="row">
+                    <div className="col-md-4 block">
+                    <div className='users' key={i}>
+                        <Link to={`/users/${user._id}`} style={{color: 'red'}} activeStyle={{color: 'white'}}><h1 class="circle" align="center">{user.username}</h1></Link>
+                    </div>
+                    </div>
+                 </div>
+                 </div>
+
+                ))}
+                </div>
+
+                 <button onClick={this.toggleNewUserForm}>Create new user</button>
                 {this.state.newUserFormVisible ? <NewUserForm
                     getAllUsers={this.getAllUsers}
                     toggleNewUserForm={this.toggleNewUserForm}
                     /> : null}
-                {this.state.users.map((user, i) => (
-                    <div key={i}>
-                        <h1>{user.username}</h1>
-                        <Link to={`/users/${user._id}`} ><h3>{user.username}</h3></Link>
-                    </div>
-                ))}
+
             </GeneralStyles>
         );
     }
